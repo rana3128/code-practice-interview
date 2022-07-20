bb3b697c0355ead1ffaac8a9d3427ee0395d68dd
vijay <rana3128@gmail.com>
2022-07-21 01:58:05 +0530

    update 20-July

---
+++ test2.js
@@ -0,0 +1,29 @@
+// const str = "aababcbacd";
+const str = "aabbabcedd";
+
+function largestUnique(str) {
+    let start = 0;
+    let lSubStr = "";
+    let hmap = {};
+
+    for (let i = 0; i <= str.length; i++) {
+        if (hmap[str[i]] || !str[i]) {
+
+            if (i - start > lSubStr.length) {
+                lSubStr = str.substring(start, i);
+            }
+
+            while(start < i && str[i] != str[start]){
+                delete hmap[str[start]];
+                start++;
+            }
+            start++;
+        } else {
+            hmap[str[i]] = true;
+        }
+    }
+
+    return lSubStr;
+}
+
+console.log(largestUnique(str));
