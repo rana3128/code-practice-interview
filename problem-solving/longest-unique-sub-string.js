5949075337b44fedfda9a77c24bb5fd563af403c
vijay pratap singh <rana3128@gmail.com>
2023-10-30 19:33:31 +0530

    Update longest-unique-sub-string.js

--- problem-solving/longest-unique-sub-string.js
+++ problem-solving/longest-unique-sub-string.js
@@ -27,3 +27,44 @@ function largestUnique(str) {
 }
 
 console.log(largestUnique(str));
+
+
+
+// Second approach
+// Input: s = "abcabcbb" Output: 3
+// Input: s = "pwwkew" Output: 3
+// Input: s = "bbbbb" Output: 1
+
+const input = "bbbbb"
+
+function LargestNonRepeted(str){
+  let start = 0;
+  let end = 1;
+  let tmpStr = str[0];
+  let result = ""
+
+  while(start < end){
+
+    if(tmpStr.includes(str[end])){
+      
+      if( result.length < tmpStr.length ){
+        result = tmpStr;
+      }
+
+      while( str[start] !== str[end] ) {
+        start++;
+      }
+      start++;
+
+      tmpStr = str.substring(start, end)
+    } else {
+      tmpStr += str[end]
+      end++;
+    }
+  }
+
+  return result.length;
+
+}
+
+console.log(LargestNonRepeted(input));
